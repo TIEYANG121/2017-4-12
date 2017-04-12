efe8d06266d9e7d452787c4a54b40fbfd44b2c26
Your Name <you@example.com>
2017-04-12 20:58:43 +0800

    demo

---
+++ biaoge1/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('public'));
+app.set('view engine','jade');
+
+var students=[
+	{name:"小明",age:2,sex:"female"},
+	{name:"小红",age:3,sex:"male"},
+	{name:"小蓝",age:5,sex:"female"},
+	{name:"小紫",age:8,sex:"male"},
+	{name:"小白",age:19,sex:"male"},
+	{name:"超人",age:48,sex:"female"},
+	{name:"蝙蝠侠",age:30,sex:"female"}
+]
+app.post("/insert",function(req,res){
+	students.push(req.body);
+	res.redirect("/")
+})
+app.get('/',function(req,res){
+	res.render('each',{student:students})
+	console.log(students)
+})
+app.listen(8080,function(){
+	console.log('server is running');
+})
